7d308b5fff7cc265787ffbbba4e034443c284814
Alex-G-R <alex.grabski555@gmail.com>
2023-09-07 18:57:02 +0200

    Adding comments

--- binary_search.py
+++ binary_search.py
@@ -4,11 +4,12 @@
 #
 
 def binary_search(list, item):
-    
+    # Low is declared as the first element of the array
     low = 0
-
-    
+    # High is declared as the last element of the array
     high = len(list)-1
+
+    # We will use high and low to control witch part of the array is being search
 
     while low <= high:
         mid = (low + high) / 2
@@ -25,5 +26,3 @@ my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20
 
 print(binary_search(my_list, 2))
 print(binary_search(my_list, -300))
-
-    
